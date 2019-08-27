4480261c9a65ab3fa22e5441ea1a7bab68f1ab0e
OwnGhy <919986426@qq.com>
2019-08-27 14:45:30 +0800

    删除注释

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,6 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import blogs from './../../blog.json';
 import {formatByMarked} from "@utils/tools";
-import path from 'path';
 
 Vue.use(Vuex);
 
@@ -19,7 +18,7 @@ const state = {
 };
 
 function escape2Html(str) {
-    var arrEntities={'lt':'<','gt':'>','nbsp':' ','amp':'&','quot':'"', '#x60': '`'};
+    let arrEntities={'lt':'<','gt':'>','nbsp':' ','amp':'&','quot':'"', '#x60': '`'};
     return str.replace(/&(lt|gt|nbsp|amp|quot|#x60);/ig,function(all,t){
         return arrEntities[t];
     });
@@ -27,17 +26,6 @@ function escape2Html(str) {
 
 const actions = {
     getAllBlogContent({ commit }) {
-        // const blogPromise = blog.map(b => {
-        //     // import(b.path)不行，是因为webpack的import()不能完全实现动态导入，不能传入变量，必须是字符串
-        //     // import(`./../../${b.path}`)不行，虽然是字符串了，但是没有标明一个统一的目录
-        //     // webpack会根据标明的目录统一将该目录下的文件进行单独打包，以实现动态导入
-        //     return import(`./../../publishers/${b.path.split('/publishers/')[1]}`);
-        // });
-        //
-        // Promise.all(blogPromise).then(res => {
-        //
-        // });
-
         const blogWithContent = blog.map((b) => ({
             ...b,
             ...formatByMarked(escape2Html(b.outline))
